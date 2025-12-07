7e67f80e01ed1b4810d895c5c1ac4eb0f5c6ab2b
KuKaH <kukah99@gmail.com>
2025-12-08 05:31:23 +0900

    [feat] 폰트 및 컬러 추가

---
+++ BaeMinSwiftUI/BaeMinSwiftUI/Resources/Extension/Fonts+.swift
@@ -0,0 +1,23 @@
+//
+//  Fonts+.swift
+//  BaeMinSwiftUI
+//
+//  Created by 홍준범 on 12/8/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let head_b_24: Font = .custom("Pretendard-Bold", size: 24)
+    static let head_b_18: Font = .custom("Pretendard-Bold", size: 18)
+    static let head_b_16: Font = .custom("Pretendard-Bold", size: 16)
+    static let head_b_14: Font = .custom("Pretendard-Bold", size: 14)
+    static let head_b_13: Font = .custom("Pretendard-Bold", size: 13)
+    static let title_sb_18: Font = .custom("Pretendard-SmiBold", size: 18)
+    static let title_sb_14: Font = .custom("Pretendard-SmiBold", size: 14)
+    static let title_sb_10: Font = .custom("Pretendard-SmiBold", size: 10)
+    static let body_r_14: Font = .custom("Pretendard-Regular", size: 14)
+    static let body_r_13: Font = .custom("Pretendard-Regular", size: 13)
+    static let body_r_12: Font = .custom("Pretendard-Regular", size: 12)
+    static let body_r_10: Font = .custom("Pretendard-Regular", size: 10)
+}
